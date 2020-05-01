540479623bd77a87273d1e4cf4fb250a75b09583
Gretchen Harris <gretchenh77@yahoo.com>
2020-05-01 12:50:16 -0400

    finished activity

---
+++ assets/javascript/counter.js
@@ -0,0 +1,34 @@
+// The best-practice is to pull all the elements into variables, in order to avoid
+// searching the HTML tree for these elements more than once:
+let incrementButton = document.querySelector("#increment"); 
+let decrementButton = document.querySelector("#decrement"); 
+let counter = document.querySelector("#counter");
+
+// This line says, "I want to listen for someone to CLICK my
+// incrementButton, and any time I 'hear' that event occur,
+// I want some code to run.
+incrementButton.addEventListener("click", function() {
+    // Any code you write in here, will run every time incrementButton
+    // is clicked.
+    // Run the console.log function to display some text in the console:
+    console.log("+ button clicked");
+    let newCounterValue = Number(counter.innerHTML) + 1;
+    counter.innerHTML = newCounterValue;
+    if(newCounterValue >= 10) {         // WHAT IS THIS? Check out "IF Statements" in the Useful Resources section.
+    counter.style.color = "red";
+    }
+  }) // We have to close the curly braces and parentheses we opened above.
+
+  decrementButton.addEventListener("click", function() {
+    console.log("- button clicked");
+    
+    // Only update the counter value on the screen if the counter is > 0:
+    if (counter.innerHTML > 0) {
+        let newCounterValue = Number(counter.innerHTML) - 1;
+        counter.innerHTML = newCounterValue;
+    if(newCounterValue < 10){
+            counter.style.color = "black";
+          }
+    }
+  })
+
